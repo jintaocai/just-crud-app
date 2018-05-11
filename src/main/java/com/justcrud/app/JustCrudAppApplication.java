ccd1fcad868ccee5d3602fb4afcdf9be0a1dd210
caijintao <caijintao@gd.e-chinalife.com>
2018-05-11 17:30:44 +0800

    初始化项目

---
+++ src/main/java/com/justcrud/app/JustCrudAppApplication.java
@@ -0,0 +1,12 @@
+package com.justcrud.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JustCrudAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JustCrudAppApplication.class, args);
+	}
+}
